dc95a257918dd223f6ed1e9a41c9d5f8dcaf14d3
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-06-29 18:10:55 +0300

    refactor: изменить список адресов CORS

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -1,13 +1,8 @@
 const ALLOWED_CORS = [
   'http://mesto.kush-mash.nomoredomains.rocks',
   'https://mesto.kush-mash.nomoredomains.rocks',
-  'http://api.mesto.kush-mash.nomoreparties.sbs',
-  'https://api.mesto.kush-mash.nomoreparties.sbs',
-  'http://158.160.74.38',
-  'https://158.160.74.38',
   'http://localhost:3000',
   'http://localhost:3001',
-  '*',
 ];
 
 module.exports = { ALLOWED_CORS };
